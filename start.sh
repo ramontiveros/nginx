86ba025f16d20bc670f4e4b2f7f55eff650a2c6d
ramontiveros <rontiveros@sitek.mx>
2019-12-02 18:06:49 -0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 CONFIG=/root/config/config.sh
+TMP_CFG=/tmp/config.sh
 
 if [ -f $CONFIG ]; then
-   chmod +x $CONFIG
-   $CONFIG
-   rm $CONFIG
+   cp $CONFIG $TMP_CFG
+   chmod +x $TMP_CFG
+   $TMP_CFG
+   rm $TMP_CFG
 fi
 
 /usr/sbin/sshd -D
